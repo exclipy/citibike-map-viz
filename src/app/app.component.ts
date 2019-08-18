59e066f82716ac4d6e29ef6c8e6043fd004ab168
Kevin Wu Won <kww@fb.com>
2019-08-18 19:13:05 -0400

    Example Leaflet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild, AfterViewInit, ElementRef} from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'citibike-map-viz';
+  options = {
+    layers: [
+      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: '...',
+      }),
+    ],
+    zoom: 5,
+    center: L.latLng(46.879966, -121.726909),
+  };
+  layers = [
+    L.circle([46.95, -122], {radius: 5000}),
+    L.polygon([[46.8, -121.85], [46.92, -121.92], [46.87, -121.8]]),
+    L.marker([46.879966, -121.726909]),
+  ];
+
+  constructor() {}
 }
