1502f46a3be32188c992f5ca5edb869ca2dd4a3e
Kevin Wu Won <kww@fb.com>
2019-08-18 23:41:33 -0400

    Make the slider work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter} from '@angular/core';
 import {scale} from 'chroma-js';
 import * as L from 'leaflet';
 import {flatMap} from 'lodash';
-import {StationData} from '../station_data';
-import {StationInformation} from '../station_information';
+import * as StationData from '../station_data';
+import * as StationInformation from '../station_information';
+import {MatSliderChange} from '@angular/material/slider';
 
 @Component({
   selector: 'app-root',
@@ -22,24 +23,42 @@ export class AppComponent {
     center: L.latLng(40.7583757, -73.9736026),
   };
   layers = [] as L.Circle[];
+  slices = [] as L.Circle[][];
   sliderMax = 0;
+  displayedTime = '';
 
   constructor() {
     this.fetchStations();
   }
 
-  onSlide() {}
+  onSlide(e: MatSliderChange) {
+    this.handleSlide(e.value);
+  }
+
+  handleSlide(value: number) {
+    this.layers = this.slices[value];
+  }
 
   async fetchStations() {
-    const info = await this.fetchJson<StationInformation>('/assets/station_information.json');
-    const data = await this.fetchJson<StationData>('/assets/data.json');
+    const info = await this.fetchJson<StationInformation.StationInformation>(
+      '/assets/station_information.json',
+    );
+    const data = await this.fetchJson<StationData.StationData>('/assets/data.json');
     if (!data.length) {
       return;
     }
     this.sliderMax = data.length - 1;
     const stationMap = new Map(info.data.stations.map(station => [station.station_id, station]));
-    const latestStations = data[0].data;
-    const joinedLatestStations = flatMap(latestStations, station => {
+
+    this.slices = data.map(d => this.toCircles(d.data, stationMap));
+    this.handleSlide(0);
+  }
+
+  private toCircles(
+    latestStations: StationData.Station[],
+    stationMap: Map<string, StationInformation.Station>,
+  ) {
+    const joinedLatestStations = flatMap(latestStations, (station: StationData.Station) => {
       const stationInfo = stationMap.get(station.station_id);
       if (stationInfo) {
         return [
@@ -53,10 +72,8 @@ export class AppComponent {
         return [];
       }
     });
-
     const colorScale = scale(['red', 'yellow', 'green']);
-
-    this.layers = joinedLatestStations.map(station => {
+    const circles = joinedLatestStations.map(station => {
       const l =
         station.num_bikes_available / (station.num_bikes_available + station.num_docks_available);
       const color = colorScale(l).hex();
@@ -67,6 +84,7 @@ export class AppComponent {
         stroke: false,
       });
     });
+    return circles;
   }
 
   private async fetchJson<T>(url: string) {
