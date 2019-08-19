b7470e66b5c4820e0f1b7877fd6882d20bf49ab7
Kevin Wu Won <kww@fb.com>
2019-08-18 23:08:57 -0400

    Install material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {LeafletModule} from '@asymmetrik/ngx-leaflet';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, LeafletModule.forRoot()],
+  imports: [BrowserModule, LeafletModule.forRoot(), BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
