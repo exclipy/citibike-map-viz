a95aaed5058ee66231220ba728903093ce50f19c
Kevin Wu Won <kww@fb.com>
2019-08-19 22:21:44 -0400

    Nicer layout on smalll screens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
-import {Component, EventEmitter} from '@angular/core';
+import {BreakpointObserver} from '@angular/cdk/layout';
+import {Component} from '@angular/core';
+import {MatSliderChange} from '@angular/material/slider';
 import {scale} from 'chroma-js';
 import * as L from 'leaflet';
 import {flatMap} from 'lodash';
+import * as moment from 'moment-timezone';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 import * as StationData from '../station_data';
 import * as StationInformation from '../station_information';
-import {MatSliderChange} from '@angular/material/slider';
-import * as moment from 'moment-timezone';
 
 @Component({
   selector: 'app-root',
@@ -26,11 +29,16 @@ export class AppComponent {
   layers = [] as L.Circle[];
   slices = [] as L.Circle[][];
   times = [] as string[];
+  sliderValue = 0;
   sliderMax = 0;
   displayedTime = '';
+  isWidescreen$: Observable<boolean>;
 
-  constructor() {
+  constructor(private breakpointObserver: BreakpointObserver) {
     this.fetchStations();
+    this.isWidescreen$ = this.breakpointObserver
+      .observe(['(min-width: 700px)'])
+      .pipe(map(state => state.matches));
   }
 
   onSlide(e: MatSliderChange) {
@@ -38,8 +46,8 @@ export class AppComponent {
   }
 
   handleSlide(value: number) {
-    this.layers = this.slices[value];
-    this.displayedTime = this.times[value];
+    this.layers = this.slices[this.sliderMax - value];
+    this.displayedTime = this.times[this.sliderMax - value];
   }
 
   async fetchStations() {
@@ -52,7 +60,6 @@ export class AppComponent {
     if (!data.length) {
       return;
     }
-    this.sliderMax = data.length - 1;
     const stationMap = new Map(info.data.stations.map(station => [station.station_id, station]));
 
     this.slices = data.map(d => this.toCircles(d.data, stationMap));
@@ -62,7 +69,9 @@ export class AppComponent {
         .tz('America/New_York')
         .format('hA, ddd MMM D YYYY');
     });
-    this.handleSlide(0);
+    this.sliderMax = data.length - 1;
+    this.sliderValue = this.sliderMax;
+    this.handleSlide(this.sliderValue);
   }
 
   private toCircles(
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {LeafletModule} from '@asymmetrik/ngx-leaflet';
 import {AppComponent} from './app.component';
+import {LayoutModule} from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,6 +15,7 @@ import {AppComponent} from './app.component';
     BrowserAnimationsModule,
     MatSliderModule,
     MatToolbarModule,
+    LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
